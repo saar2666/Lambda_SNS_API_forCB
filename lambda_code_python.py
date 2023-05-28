8ce8f534fdc94a6dcadbf32ca863245a6f4db911
Saar Azulay <48453080+saar2666@users.noreply.github.com>
2023-05-28 20:48:28 +0300

    1

--- lambda_code_python.py
+++ lambda_code_python.py
@@ -1,27 +1,45 @@
 import json
+import boto3
 
 def lambda_handler(event, context):
-    # take the numbers from the event body
-    body = json.loads(event['body'])
-    number1 = body.get('number1')
-    number2 = body.get('number2')
+    try:
+        # Take numbers from event body
+        body = event
 
-    if number1 is None or number2 is None:
+        number1 = body.get('number1')
+        number2 = body.get('number2')
+
+        if number1 is None or number2 is None:
+            return {
+                'statusCode': 400,
+                'body': json.dumps({'error': 'Invalid payload'})
+            }
+
+        # Calculate sum
+        result = number1 + number2
+
+        # Prepare response
+        response = {
+            'statusCode': 200,
+            'body': json.dumps({'result': result}),
+            'headers': {
+                'Content-Type': 'application/json'
+            }
+        }
+
+        # Send response to SNS topic
+        sns = boto3.client('sns')
+        sns.publish(
+            TopicArn='arn:aws:sns:eu-north-1:633172536382:saar1',
+            Message=json.dumps(response)
+        )
+
+        return response
+
+    except Exception as e:
+        # Handle exceptions
         return {
-            'statusCode': 400,
-            'body': json.dumps({'error': 'Invalid payload'})
+            'statusCode': 500,
+            'body': json.dumps({'error': str(e)})
         }
 
-    # calculate sum
-    result = number1 + number2
-
-    # prepare response
-    response = {
-        'statusCode': 200,
-        'body': json.dumps({'result': result}),
-        'headers': {
-            'Content-Type': 'application/json'
-        }
-    }
-
-    return response
